465e39f929dd9f7d11ea594e9623525618fa329d
SteppenWolf <SteppenWolf.unity@gmail.com>
2017-07-05 22:10:11 +0300

    added player prefs

---
+++ Assets/Scripts/UserDataController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class UserDataController : SingletonePattern<UserDataController> {
+
+    //private 
+
+
+    public void SaveAll()
+    {
+        
+    }
+
+    public void SaveUser()
+    {
+        
+    }
+
+    public void SaveCar()
+    {
+        
+    }
+
+    public void SaveMap()
+    {
+        
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
