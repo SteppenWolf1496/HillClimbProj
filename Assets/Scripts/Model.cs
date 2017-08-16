a5685dcc4cd7b713ba49782ad99473f8ab26d3a9
Surkov <SteppenWolf.unity@gmail.com>
2017-08-16 20:18:33 +0300

    some magic changes

--- Assets/Scripts/Model.cs
+++ Assets/Scripts/Model.cs
@@ -1,11 +1,40 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts;
+using CodeStage.AntiCheat.ObscuredTypes;
 
 public class Model : MonoBehaviour
 {
 
     public static int curCarIndex = 0;
-    public static string[] cars = new[] {"car", "monster"};
+    private static List<BaseCarData> cars = new List<BaseCarData>();
+    public static List<UserCarData> ucars = new List<UserCarData>();
+   // UserMaps Data
+    public static ObscuredInt BlackMetal;
+    public static ObscuredInt GoldMetal;
+
+    public static List<BaseCarData> BaseCars
+    {
+        get
+        {
+            if (cars.Count == 0)
+            {
+                //ZALIPUHA POKA NETU ADMINKI
+                BaseCarData tmp = new BaseCarData();
+                tmp.key = "sedan";
+                cars.Add(tmp);
+                tmp = new BaseCarData();
+                tmp.key = "monster";
+                cars.Add(tmp);
+            }
+
+            return cars;
+        }
+       
+    }
+
+    //public static
 
 }
 
---
+++ Assets/Scripts/UserCarData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CodeStage.AntiCheat.ObscuredTypes;
+
+namespace Assets.Scripts
+{
+    class UserCarData
+    {
+        public ObscuredString key;
+
+        //public ObscuredInt 
+
+    }
+}
